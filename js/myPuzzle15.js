f87e4a111bb9f676415d5612aa0c922e522ba6f8
frontendoleg <frontendoleg@gmail.com>
2016-05-28 20:20:11 +0300

    I work with mix method

--- js/myPuzzle15.js
+++ js/myPuzzle15.js
@@ -1,13 +1,13 @@
 var arr = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]];
 function getNull(){	//функция возвращает координату пустой клетки
-				for (var yi = 0;  yi < 4; yi++) {
-					for (var xj = 0; xj < 4; xj++) {
-						if (arr[yi][xj] === 0) {
-							return{"x":xj,"y":yi};
-						}
-					}
-				}
+	for (var yi = 0;  yi < 4; yi++) {
+		for (var xj = 0; xj < 4; xj++) {
+			if (arr[yi][xj] === 0) {
+				return{"x":xj,"y":yi};
 			}
+		}
+	}
+}
 
 // возвращает размер клиентской области окна
 function GetWindowClientSize() {
@@ -28,7 +28,7 @@ function GetWindowClientSize() {
 		(document.parentWindow||document.defaultView).innerHeight;
 
 	return {width:ClientWidth, height:ClientHeight};
-	}
+}
 
 function init() {
 	console.time("draw");
@@ -36,70 +36,74 @@ function init() {
 	var boneSize = 10;  // размер костяшки vmin /////////////////////////////////////   было 20
 
 	////определяем ширину и высоту клиентской области окна
-				var ClientWidth = GetWindowClientSize().width;
-				var ClientHeight = GetWindowClientSize().height;
-				console.log('clientWidth = ', ClientWidth);
-				console.log('clientHeight = ', ClientHeight);
-makeGameField(fieldSize); // создаем игровое поле///////////////////////////////////////////////
+	var ClientWidth = GetWindowClientSize().width;
+	var ClientHeight = GetWindowClientSize().height;
+	console.log('clientWidth = ', ClientWidth);
+	console.log('clientHeight = ', ClientHeight);
 
-var field = new game15(); 	// создаем объект пятнашек
+	makeGameField(fieldSize); // создаем игровое поле///////////////////////////////////////////////
 
-field.draw(boneSize);
-// console.log ('point48');
-console.timeEnd("draw"); // рисуем наши пятнашки
+	var field = new Game15(); 	// создаем объект пятнашек
 
-function event(x,y){ // функция действия при кликах мыши
-	field.move(x,y);
-	field.draw(boneSize);
-}
+	field.draw(boneSize); // ------- рисуем наши пятнашки на экране --------------
+	// console.log ('point48');
+	console.timeEnd("draw"); // рисуем наши пятнашки
 
-function eventKey(x,y){ // функция действия при нажатии клавиш
-	// нужно переместить подходящую костяшку, если это возможно
-	var xNull = getNull().x;
-	// console.log ('xNull = ',xNull);
-	var yNull = getNull().y;
-	// console.log ('yNull = ',yNull);
-	var xBone = x+xNull, yBone = y+yNull; // координаты клетки кандидата
-	// console.log('xBone = ', xBone);
-	// console.log('yBone = ', yBone);
-	if((xBone >= 0 && xBone < 4) && (yBone >= 0 && yBone < 4)){
-		// костяшку сдвинуть можно
-		//alert('xBone = '+xBone + ' yBone = '+yBone);
-		field.move(xBone,yBone);
-	field.draw(boneSize);
+	field.mix(100); // перемешиваем
 
+	function event(x,y){ // функция действия при кликах мыши
+		field.move(x,y);
+		field.draw(boneSize);
 	}
-}
 
-$(function(){ // обработчик кликов мыши
-	var fieldSpace = $('#fff');
-	fieldSpace.click(function(e){
-		var br = this.getBoundingClientRect(); // br - это область клиента, на которую произвели клик, то есть игровое поле
-		var myField = br.right - br.left; // это ширина игрового поля в пикселах
-		var myBone = Math.floor(myField/4); // это размер костяшки в пикселах. Мы знаем, что игровое поле 80vmin а костяшка 20vmin
-		var x = ( e.pageX - fieldSpace.offset().left) / myBone | 0; // координата костяшки в пределах 0-3
-		var y = ( e.pageY - fieldSpace.offset().top) / myBone | 0; // координата костяшки в пределах 0-3
-		event(x,y); // вызов функции действия
+	function eventKey(x,y){ // функция действия при нажатии клавиш
+		// нужно переместить подходящую костяшку, если это возможно
+		var xNull = getNull().x;
+		// console.log ('xNull = ',xNull);
+		var yNull = getNull().y;
+		// console.log ('yNull = ',yNull);
+		var xBone = x+xNull, yBone = y+yNull; // координаты клетки кандидата
+		// console.log('xBone = ', xBone);
+		// console.log('yBone = ', yBone);
+		if((xBone >= 0 && xBone < 4) && (yBone >= 0 && yBone < 4)){
+			// костяшку сдвинуть можно
+			//alert('xBone = '+xBone + ' yBone = '+yBone);
+			field.move(xBone,yBone);
+		field.draw(boneSize);
 
-	});
-});
-
-$(function(){ // обработчик клавиатуры
-	var body = $('body');
-	body.keydown(function(e){
-		switch (e.which) {
-			case 37 : eventKey(1,0);break; // хотим сместить костяшку с правой позиции (+1) горизонтали и на 0 по вертикали
-			case 39 : eventKey(-1,0);break; // хотим сместить костяшку с левой позиции (-1) по горизонтали
-			case 38 : eventKey(0,1);break; // хотим сместит сверху по вертикали
-			case 40 : eventKey(0,-1); // хотим сместить снизу по вертикали
 		}
+	}
+
+	$(function(){ // обработчик кликов мыши
+		var fieldSpace = $('#fff');
+		fieldSpace.click(function(e){
+			var br = this.getBoundingClientRect(); // br - это область клиента, на которую произвели клик, то есть игровое поле
+			var myField = br.right - br.left; // это ширина игрового поля в пикселах
+			var myBone = Math.floor(myField/4); // это размер костяшки в пикселах. Мы знаем, что игровое поле 80vmin а костяшка 20vmin
+			var x = ( e.pageX - fieldSpace.offset().left) / myBone | 0; // координата костяшки в пределах 0-3
+			var y = ( e.pageY - fieldSpace.offset().top) / myBone | 0; // координата костяшки в пределах 0-3
+			event(x,y); // вызов функции действия
+
+		});
 	});
 
-});
+	$(function(){ // обработчик клавиатуры
+		var body = $('body');
+		body.keydown(function(e){
+			switch (e.which) {
+				case 37 : eventKey(1,0);break; // хотим сместить костяшку с правой позиции (+1) горизонтали и на 0 по вертикали
+				case 39 : eventKey(-1,0);break; // хотим сместить костяшку с левой позиции (-1) по горизонтали
+				case 38 : eventKey(0,1);break; // хотим сместит сверху по вертикали
+				case 40 : eventKey(0,-1); // хотим сместить снизу по вертикали
+			}
+		});
+
+	});
+
+} // the end of the init function
 
 
-}
-function game15(){
+function Game15(){
 	var cellView = null;
 	var numView = null;
 
@@ -187,25 +191,32 @@ function game15(){
 	function getRandomBool() {
 		if (Math.floor(Math.random() * 2) === 0) {
 			return true;
-		}
+		} else {return false;}  // ----------------------------------------костыль
 	};  // --> end of getRandomBool function
 	// метод перемешивает пятнашки
 	this.mix = function(stepCount) {
+		console.log('mix starts', stepCount);
 		var x,y;
 		for (var i = 0; i < stepCount; i++) {
-			var nullX = getNull.x;
-			var nullY = getNull.y;
+			console.log('i=',i);
+			var nullX = getNull().x;
+			var nullY = getNull().y;
+			console.log('nullX=', nullX,' nullY=',nullY);
 			var hMove = getRandomBool();
 			var upLeft = getRandomBool();
+			console.log('hMove=',hMove,' upLeft=',upLeft);
 			if (!hMove && !upLeft) { y = nullY; x = nullX - 1;}
 			if (hMove && !upLeft) { x = nullX; y = nullY + 1;}
 			if (!hMove && upLeft) { y = nullY; x = nullX + 1;}
 			if (hMove && upLeft) { x = nullX; y = nullY - 1;}
 			if (0 <= x && x <= 3 && 0 <= y && y <=3) {
+				console.log('Coordinats for move x=',x,' y=',y);
 				this.move(x,y);
+				this.draw(10);
 			}
 		}
 	clicks = 0;
+	console.log('why clicks is not zero', clicks);
 	}; //  --> end of this.mix method
 }
 
